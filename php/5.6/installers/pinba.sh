69a4a2f0a6ad0bc58722a66d5d6c00c873c52c3b
Serghei Iakovlev <sadhooklay@gmail.com>
2016-04-13 21:15:32 +0300

    Fixed PHP 5.6 build

--- php/5.6/installers/pinba.sh
+++ php/5.6/installers/pinba.sh
@@ -13,4 +13,4 @@ make install
 mv /tmp/pinba-ext/pinba_extension/modules/pinba.so "${PHP_EXT_DIR}/pinba.so"
 
 touch /etc/php5/mods-available/pinba.ini && echo extension=pinba.so > /etc/php5/mods-available/pinba.ini
-ln -s /etc/php5/mods-available/pinba.ini /etc/php5/cli/conf.d/30-pinba.ini
+ln -s /etc/php5/mods-available/pinba.ini /etc/php5/cli/conf.d/20-pinba.ini
