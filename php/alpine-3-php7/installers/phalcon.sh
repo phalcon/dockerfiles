fa5d79bd50b63ebd792442102ee70db92750187c
Serghei Iakovlev <serghei@phalconphp.com>
2017-03-02 12:25:50 +0200

    Improved php/alpine-3-php7

--- php/alpine-3-php7/installers/phalcon.sh
+++ php/alpine-3-php7/installers/phalcon.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-git clone -q https://github.com/phalcon/cphalcon.git /tmp/phalcon-ext
+# Use stable version
+git clone --depth=1 -v https://github.com/phalcon/cphalcon.git /tmp/phalcon-ext
 cd /tmp/phalcon-ext/build
 
 ./install
