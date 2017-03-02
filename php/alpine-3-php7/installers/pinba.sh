1bfa0c0bd104bd11fe1c4e680d51e6ef1337b1c2
Serghei Iakovlev <serghei@phalconphp.com>
2017-03-02 14:38:03 +0200

    Amended php/alpine-3-php7

---
+++ php/alpine-3-php7/installers/pinba.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+git clone --depth=1 -v https://github.com/tony2001/pinba_extension.git /tmp/pinba_extension
+cd /tmp/pinba_extension
+
+phpize &> /dev/null
+./configure --silent --enable-pinba &> /dev/null
+
+make --silent -j"$(getconf _NPROCESSORS_ONLN)" &> /dev/null
+make --silent install
+
+echo "extension=pinba.so" > /etc/php7/conf.d/20-pinba.ini
